377ab2117b0d60bcf98d783fb2bf7c9cb7c6c671
ZuraJeladze2 <zurajeladze2@gmail.com>
2024-02-20 12:19:08 +0400

    skills section
    added navbar and skills section separately

---
+++ ZJ2/src/app/typed/typed.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-typed',
+  templateUrl: './typed.component.html',
+  styleUrl: './typed.component.scss'
+})
+export class TypedComponent {
+  count = 0;
+}
